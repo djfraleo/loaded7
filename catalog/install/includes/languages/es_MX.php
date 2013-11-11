1649f85a045a04e15bacb84a892d2e3c549d5a86
djfraleo <djfraleo@hotmail.com>
2013-11-11 13:57:03 -0600

    Create es_MX.php

---
+++ catalog/install/includes/languages/es_MX.php
@@ -0,0 +1,14 @@
+#  $Id: en_US.php v1.0 2012-12-08 datazen $
+#
+#  LoadedCommerce, Innovative eCommerce Solutions
+#  http://www.loadedcommerce.com
+#
+#  Copyright (c) 2012 Loaded Commerce, LLC
+#
+#  @author     LoadedCommerce Team
+#  @copyright  (c) 2012 LoadedCommerce Team
+#  @license    http://loadedcommerce.com/license.html
+
+image_button_continue = Continuar
+image_button_cancel = Cancelar
+image_button_retry = Reintentar
