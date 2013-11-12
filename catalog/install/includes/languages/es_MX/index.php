2fb57b7f62606f721b207ecd7a56d128dc52df18
djfraleo <djfraleo@hotmail.com>
2013-11-11 23:30:58 -0600

    Create index.php

---
+++ catalog/install/includes/languages/es_MX/index.php
@@ -0,0 +1,51 @@
+#  $Id: index.php v1.0 2012-12-08 datazen $
+#
+#  LoadedCommerce, Innovative eCommerce Solutions
+#  http://www.loadedcommerce.com
+#
+#  Copyright (c) 2012 Loaded Commerce, LLC
+#
+#  @author     LoadedCommerce Team
+#  @copyright  (c) 2012 LoadedCommerce Team
+#  @license    http://loadedcommerce.com/license.html
+
+page_title_welcome = Bienvenido a Loaded Commerce v%s
+
+page_heading_server_requirements = Requerimientos del Servidor
+page_heading_installation_type = Tipo de Instalacion
+page_heading_permissions = Permisos de Carpeta/Archivo
+
+title_language = Idioma:
+
+text_welcome = Loaded Commerce es la siguiente generacion en las plataformas de ecommerce de codigo abierto auto-hospedada y esta disponible gratuitamente bajo la Licencia Publica General GNU. Un rico conjunto de caracteristicas y funcionalidades permiten a los dueños de tiendas configurar, correr, y mantener tiendas online con minimo esfuerzo y costo minimo.
+
+text_under_development = Loaded Commerce v%s esta actualmente bajo desarrollo y no contiene todas sus caracteristicas planeadas. Esta version esta recomendada para los desarrolladores y usuarios quienes estan participando en su desarrollo y proveen retroalimentacion.
+
+box_server_title = Capacidades del Servidor
+box_server_php_version = Version PHP
+box_server_php_settings = Parametros de PHP
+box_server_register_globals = register_globals
+box_server_magic_quotes = magic_quotes
+box_server_file_uploads = file_uploads
+box_server_session_auto_start = session.auto_start
+box_server_session_use_trans_sid = session.use_trans_sid
+box_server_post_max_size = post_max_size
+post_max_size_text = 10M
+box_server_upload_max_filesize = upload_max_filesize
+upload_max_filesize_text = 10M
+box_server_php_extensions = Extensiones PHP
+box_server_mysqli = MySQLi
+box_server_gd = GD
+box_server_curl = cURL
+box_server_openssl = OpenSSL
+box_server_on = Activado
+box_server_off = Desactivado
+box_server_writeable = Escribible
+box_server_not_writeable = No Escribible
+
+error_configuration_file_not_writeable = El servidor web no parece ser capaz de escribir los parametros de la tienda online a su archivo de configuracion debido a problemas de permisos de archivos.  El archivo de configuracion esta localizado en: %s
+error_configuration_file_alternate_method = Alternativamente usted puede copiar los parametros de configuracion al archivo de configuracion a mano.  Este proceso intentara actualizar el archivo de configuracion automaticamente al final de la instalacion.
+error_javascript_disabled = Se requiere de Javascript para el procedimiento de instalacion y administracion de esta instalacion. Por favor habilitelo en su navegador y <a href="index.php">vuelva a correr el procedimiento de instalacion</a>.
+
+image_button_install = Nueva Instalacion
+image_button_upgrade = Actualizar de la version 6.x
